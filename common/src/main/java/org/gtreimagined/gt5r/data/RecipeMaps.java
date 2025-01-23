296339211b238c086742f2db010aff39260b0622
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-01-22 23:25:17 -0500

    improved info of heat exchanger recipes

--- common/src/main/java/org/gtreimagined/gt5r/data/RecipeMaps.java
+++ common/src/main/java/org/gtreimagined/gt5r/data/RecipeMaps.java
@@ -257,7 +257,7 @@ public class RecipeMaps {
             String duration = "Duration: " + recipe.getDuration() + " ticks" + additional;
             String extraBurntime = "HU/t: " + recipe.getPower();
             String heatIncreaseMultiplier = "Total HU: " + (recipe.getPower() * recipe.getDuration());
-            String heatExchanger = recipe.getSpecialValue() == 1 ? "Works in both Small and Large Heat Exchangers" : (recipe.getSpecialValue() == -1 ? "Small" : "Large") + " Heat Exchanger only";
+            String heatExchanger = recipe.getSpecialValue() == 1 ? "" : (recipe.getSpecialValue() == -1 ? "Small" : "Large") + " Heat Exchanger only";
             renderString(stack, duration, fontRenderer, 5, 0, guiOffsetX, guiOffsetY);
             renderString(stack, extraBurntime, fontRenderer, 5, 10, guiOffsetX, guiOffsetY);
             renderString(stack, heatIncreaseMultiplier, fontRenderer, 5, 20, guiOffsetX, guiOffsetY);
